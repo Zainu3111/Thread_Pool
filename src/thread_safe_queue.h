8dd1196868e84e129b7803eb98593309ef5b8d65
Zain <zu4312260@gmail.com>
2026-06-05 05:33:43 -0600

    adding initial framework

---
+++ src/thread_safe_queue.h
@@ -0,0 +1,8 @@
+#ifndef QUEUE_HH
+#define QUEUE_HH
+
+#include <iostream>
+class threadsafe_queue{
+
+};
+#endif
---
+++ src/threadpool.h
@@ -0,0 +1,22 @@
+#ifndef THREADPOOL_HH
+#define THREADPOOL_HH
+
+
+#include<iostream>
+#include<vector>
+#include<atomic>
+
+class thread_pool{
+	std::atomic_bool done;
+	
+
+
+	public:
+
+
+
+
+};
+
+
+#endif
