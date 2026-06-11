fafdcb18399bf9d6f52f74ce34302dd1f2ef9b7b
Zain <zu4312260@gmail.com>
2026-06-11 02:05:08 -0600

    added basic threadpool. Need to test it.

--- testing/Basic_Threadpool_testing/testing.cpp
+++ testing/Basic_Threadpool_testing/testing.cpp
@@ -3,20 +3,42 @@
 #include "../../benchmarks/Prime.h"
 #include <chrono>
 #define BENCHMARK 1000000
-void run(){
-	thread_pool pool;
-	std::cout << "Testing threadpool" << std::endl;
-	for (int i{}; i < 100; ++i){
-		pool.submit([i](){
+#define TEST_SIZE 850
+#include <unordered_map>
+size_t test(size_t start, size_t end){
+	size_t res{};
+	for (size_t i{start}; i <= end; ++i){
+		if (is_prime(i)){
+			++res;
+		}
+	}
+	return res;
+}
 
+void run(std::unordered_map<std::thread::id, size_t>& map){
+	thread_pool pool;
+	std::mutex mux;
+	std::cout << "Testing threadpool" << std::endl;
+	size_t ciel = BENCHMARK / TEST_SIZE;
+	for (size_t i{}; i < ciel; ++i){
+		pool.submit([i, &map, &mux](){
+				size_t cur = test(i * TEST_SIZE, (i + 1) * TEST_SIZE);
+				std::lock_guard lock_gaurd(mux);
+				map[std::this_thread::get_id()] += cur;
 				});
 	}
 }
+
 int main(){
 	auto start = std::chrono::steady_clock::now();
 
 	// start code
-	run();	
+	std::unordered_map<std::thread::id, size_t> map;
+	run(map);
+	size_t res{};
+	for(auto const& [id, val] : map){
+		res += val;
+	}
 	// end code
 	auto end = std::chrono::steady_clock::now();
 
