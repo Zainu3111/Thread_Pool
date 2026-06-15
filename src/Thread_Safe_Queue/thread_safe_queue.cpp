4b3af9d9d4d184ae7cb6a4e95202b297d7af7d16
Zain <zu4312260@gmail.com>
2026-06-15 04:26:07 -0600

    built a better threadsafe queue.

---
+++ src/Thread_Safe_Queue/thread_safe_queue.cpp
@@ -0,0 +1,5 @@
+#include "./thread_safe_queue.h"
+
+void threadsafe_queue::push(T new_value){
+
+}
---
+++ src/Thread_Safe_Queue/thread_safe_queue.h
@@ -0,0 +1,77 @@
+#ifndef QUEUE_HH
+#define QUEUE_HH
+
+#include <iostream>
+#include <vector>
+#include <optional>
+#include <queue>
+#include <memory>
+#include <condition_variable>
+
+template <typename T>
+class threadsafe_queue{
+	private:
+		std::mutex mut;
+		std::condition_variable data_cond;
+		std::queue<T> data_queue;
+
+	public:
+		threadsafe_queue();
+		threadsafe_queue(const threadsafe_queue&);
+		
+		// remove assignment operator to reduce complexity
+		threadsafe_queue operator=(const threadsafe_queue) = delete;
+		
+		inline void push(T new_value){
+			std::lock_guard<std::mutex> lock(mut);
+			data_queue.push(new_value);
+			data_cond.notify_one();
+		}
+		
+		// provide a bool if value refrence provided for value else 
+		// return a shared pointer to the value;
+		bool try_pop(T& value){
+			std::lock_guard<std::mutex> lock(mut);
+			if(data_queue.empty()){
+				return false;
+			}
+			value = data_queue.front();
+			data_queue.pop();
+			return true;
+		}
+		std::shared_ptr<T> try_pop(){
+			std::lock_guard<std::mutex> lock(mut);
+			if(data_queue.empty()){
+				return nullptr;
+			}
+			std::shared_ptr<T> ptr = std::make_shared(std::move(data_queue.front()));
+			data_queue.pop();
+			return ptr;
+		}
+
+
+		// similarly for wait and pop
+		inline void wait_and_pop(T& value){
+			std::unique_lock<std::mutex> lock(mut);
+			data_cond.wait(lock, [this](){
+				return !data_queue.empty();	
+			});
+			value = data_queue.front();
+			data_queue.pop();
+		}
+		std::shared_ptr<T> wait_ant_pop(){
+			std::unique_lock<std::mutex> lock(mut);
+			data_cond.wait(lock, [this](){
+				return !data_queue.empty();	
+			});
+			std::shared_ptr<T> value = std::make_shared(std::move(data_queue.front()));
+			data_queue.pop();
+			return value;
+		}
+
+		bool empty(){
+			std::lock_guard<std::mutex> lock(mut);
+			return data_queue.empty();
+		}
+};
+#endif
