aa58024db3b04c4678e383dd0392ec9ac9a830ce
Zain <zu4312260@gmail.com>
2026-06-16 03:30:44 -0600

    done with workpool. hope it works fine.

--- src/threadpool.h
+++ src/threadpool.h
@@ -14,13 +14,12 @@ class thread_pool{
 	std::atomic_bool done;
 	threadsafe_queue<std::function<void()>> global_work_queue;	
 	std::vector<std::thread> threads;
-	std::condition_variable cv;
 
 	void worker_thread(){
-		while(!done){
+		while(true){
 			std::function<void()> task;
-			global_work_queue.wait_and_pop(task);
-			task();
+			if (!global_work_queue.wait_and_pop(task)) break;
+				task();
 		}
 	}
 	public:
@@ -42,15 +41,13 @@ class thread_pool{
 	~thread_pool(){
 		done = true;
 		global_work_queue.set_done_flag();
-		cv.notify_all();
-		int const THREAD_COUNT = std::thread::hardware_concurrency();
-		for (int i{}; i < THREAD_COUNT; ++i){
-			if(threads.at(i).joinable()) threads.at(i).join();
+		for (auto& t : threads){
+			if(t.joinable()) t.join();
 		}
 	}
 
 	void submit(std::function<void()> new_task){
-		global_work_queue.push(new_task);
+		global_work_queue.push(std::move(new_task));
 	}
 
 };
