bc1fe393bb3dbebc752feba41ba59848c3252a91
Muhammad Zain Ul Abidin <zainulab@cs.ualberta.ca>
2026-07-04 06:04:51 -0600

    running more Benchmarks.

--- tests/sequential/prime.cpp
+++ tests/sequential/prime.cpp
@@ -1,6 +1,7 @@
 #include "../../benchmarks/prime.h"
 #include <chrono>
 #include "../../src/universal.h"
+#include <iostream>
 
 int main(){
 
@@ -10,6 +11,7 @@ int main(){
 	constexpr size_t iterations = 50000000 / BENCHMARK;
 	std::string benchmark = "Sequential Prime";
 	for(size_t j{1}; j <= iterations; ++j){
+		std::cout << "Iteration Number:" << j << std::endl;
 		auto start = std::chrono::steady_clock::now();
 		res = 0;
 		for(i = 0; i <= (j * BENCHMARK); ++i){
