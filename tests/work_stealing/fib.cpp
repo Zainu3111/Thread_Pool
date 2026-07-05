118049bcca74406ceb0703d302d8830e7dd4f336
Zain <zu4312260@gmail.com>
2026-07-05 04:34:40 -0600

    cleaning up names.

--- tests/work_stealing/fib.cpp
+++ tests/work_stealing/fib.cpp
@@ -15,7 +15,7 @@ int main(){
 	const int BENCHMARK = 40;
 	const int size = 2;
 	const int THREAD_COUNT = std::thread::hardware_concurrency();
-	std::string benchmark = "Sequential Fibonacci";
+	std::string benchmark = "Work-Stealing Fibonacci";
 	for(int i{0}; i <= 10; i += size){
 		std::cout << "Iteration Number: " << i << std::endl;
 		auto start = std::chrono::steady_clock::now();
@@ -26,7 +26,7 @@ int main(){
 
 		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 	//	std::cout << end.count() << std::endl;
-		record_result(benchmark, BENCHMARK + i, THREAD_COUNT, ms.count());
+		//record_result(benchmark, BENCHMARK + i, THREAD_COUNT, ms.count());
 	}
 	return 0;
 }
