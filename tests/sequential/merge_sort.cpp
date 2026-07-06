503d18c5c1317d7bc39ae341010403b51b084d9c
Zain <zu4312260@gmail.com>
2026-07-06 03:44:26 -0600

    setting up benchmarking for merge-sort seq.

--- tests/sequential/merge_sort.cpp
+++ tests/sequential/merge_sort.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 #include "../../benchmarks/merge_sort.h"
 #include <chrono>
-#define BENCHMARK 100000000
 #include <vector>
 #include <random>
 #include <algorithm>
+#include "../../src/universal.h"
 
 int main(){
-	std::vector<int> arr(BENCHMARK);
+
+	const int BENCHMARK = 1000000;
 	unsigned int SEED = 31;
 	std::mt19937 gen(SEED);
 	std::uniform_int_distribution<int> distrib(1, 100000000);
-	std::generate(arr.begin(), arr.end(), [&](){
-			return distrib(gen);
-			});
+	std::string benchmark = "Sequential Merge-Sort";
 
-	auto start = std::chrono::steady_clock::now();
+	for(int i{1}; i <= 30; ++i){
+		std::cout << "Iteration Number: " << i << std::endl;
+		std::vector<int> arr(BENCHMARK * i);
+		std::generate(arr.begin(), arr.end(), [&](){
+				return distrib(gen);
+				});
 
-	// start code
-	serial_merge_sort(arr, 0, arr.size() - 1);	
-	// end code
-	auto end = std::chrono::steady_clock::now();
+		auto start = std::chrono::steady_clock::now();
 
-	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-	std::cout << "Total Time: " << ms.count() << " ms\n";
+		// start code
+		serial_merge_sort(arr, 0, arr.size() - 1);	
+		// end code
+		auto end = std::chrono::steady_clock::now();
+
+		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		record_result(benchmark, BENCHMARK * i, 1, ms.count());
+//		std::cout << "Total Time: " << ms.count() << " ms\n";
+	}
 	return 0;
 }
