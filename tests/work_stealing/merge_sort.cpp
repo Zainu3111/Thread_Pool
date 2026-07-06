9c9aaaa1b0a886dcf4c1d133c54c699efedb6742
Zain <zu4312260@gmail.com>
2026-07-06 04:00:58 -0600

    setting up benchmarking for merge-sort work-stealing.

--- tests/work_stealing/merge_sort.cpp
+++ tests/work_stealing/merge_sort.cpp
@@ -2,11 +2,10 @@
 #include "../../benchmarks/merge_sort.h"
 #include "../../src/threadpool.h"
 #include <chrono>
-#define BENCHMARK 10000000
 #include <vector>
 #include <random>
 #include <algorithm>
-
+#include "../../src/universal.h"
 void check(std::vector<int>& vec){
 	int cur{vec[0]};
 	for(size_t i{1}; i < vec.size(); ++i){
@@ -29,23 +28,30 @@ void run(std::vector<int>& arr){
 
 
 int main(){
-	std::vector<int> arr(BENCHMARK);
+	const int BENCHMARK = 1000000;
 	unsigned int SEED = 31;
 	std::mt19937 gen(SEED);
 	std::uniform_int_distribution<int> distrib(1, 100000000);
-	std::generate(arr.begin(), arr.end(), [&](){
-			return distrib(gen);
-			});
+	std::string benchmark = "Work-Stealing Merge-Sort";
+	const int THREAD_COUNT = std::thread::hardware_concurrency();
+	for(int i{1}; i <= 30; ++i){
+		std::cout << "Iteration Number: " << i << std::endl;
+		std::vector<int> arr(BENCHMARK * i);
+		std::generate(arr.begin(), arr.end(), [&](){
+				return distrib(gen);
+				});
 
-	auto start = std::chrono::steady_clock::now();
+		auto start = std::chrono::steady_clock::now();
 
-	// start code
-	run(arr);	
-	// end code
-	auto end = std::chrono::steady_clock::now();
-
-	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-	std::cout << "Time Taken: " << ms.count() << "ms" << std::endl; 
-	check(arr);
+		// start code
+		run(arr);	
+		// end code
+		auto end = std::chrono::steady_clock::now();
+	
+		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+		//record_result(benchmark, BENCHMARK * i, THREAD_COUNT, ms.count());
+		std::cout << "Time Taken: " << ms.count() << "ms" << std::endl; 
+		//check(arr);
+	}
 	return 0;
 }
