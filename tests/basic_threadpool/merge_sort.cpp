ebdb57cee6eda04676b95f65a5f50d12c77657ed
Zain <zu4312260@gmail.com>
2026-07-06 04:21:29 -0600

    setting up benchmarking for merge-sort basic pool.

--- tests/basic_threadpool/merge_sort.cpp
+++ tests/basic_threadpool/merge_sort.cpp
@@ -2,14 +2,14 @@
 #include "./merge_sort.h"
 #include "./threadpool.h"
 #include <chrono>
-#define BENCHMARK 100000000
 #include <vector>
 #include <random>
 #include <algorithm>
+#include "../../src/universal.h"
 
 void run(std::vector<int>& arr){
 	thread_pool pool;
-	std::cout << "Testing Merge-Sort With a Global Lock-Based Queue Threadpool" << std::endl;
+	//std::cout << "Testing Merge-Sort With a Global Lock-Based Queue Threadpool" << std::endl;
 	auto task = pool.submit([&pool, &arr](){
 			parallel_merge_sort(pool, arr, 0, arr.size() - 1);
 			});
@@ -18,27 +18,29 @@ void run(std::vector<int>& arr){
 
 
 int main(){
-	std::vector<int> arr(BENCHMARK);
+	const int BENCHMARK = 1000000;
 	unsigned int SEED = 31;
 	std::mt19937 gen(SEED);
 	std::uniform_int_distribution<int> distrib(1, 100000000);
-	std::generate(arr.begin(), arr.end(), [&](){
-			return distrib(gen);
-			});
+	std::string benchmark = "Work-Stealing Merge-Sort";
+	const int THREAD_COUNT = std::thread::hardware_concurrency();
+	for(int i{1}; i <= 30; ++i){
+		std::cout << "Iteration Number: " << i << std::endl;
+		std::vector<int> arr(BENCHMARK * i);
+		std::generate(arr.begin(), arr.end(), [&](){
+				return distrib(gen);
+				});
 
-	auto start = std::chrono::steady_clock::now();
+		auto start = std::chrono::steady_clock::now();
 
-	// start code
-	run(arr);	
-	// end code
-	auto end = std::chrono::steady_clock::now();
-
-	auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-	std::cout << "Total Time: " << ms.count() << " ms\n";
-	std::cout << "[";
-	for(size_t i{}; i < 100; ++i){
-		std::cout << arr[i] << ", ";
+		// start code
+		run(arr);	
+		// end code
+		auto end = std::chrono::steady_clock::now();
+	
+		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+	//	record_result(benchmark, BENCHMARK * i, THREAD_COUNT, ms.count());
+		std::cout << "Time Taken: " << ms.count() << "ms" << std::endl; 
 	}
-	std::cout << "]" << std::endl;
 	return 0;
 }
